4deeb33252e8e64e298063a1cb10646f05a1640e
xvii-tommy <thomas.easterbrook97@gmail.com>
2023-12-21 22:37:59 +0000

    Fixed naming convention

--- problem_A1.py
+++ problem_A1.py
@@ -1,46 +1,65 @@
-import numpy as np
+#What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the grid?
 
-# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
-# Find the sum of all the primes below two million.
+grid = [[8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
+        [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
+        [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
+        [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
+        [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
+        [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
+        [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
+        [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
+        [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
+        [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
+        [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
+        [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
+        [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
+        [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
+        [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
+        [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
+        [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
+        [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
+        [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
+        [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]]
 
-########## Brute force approach ##########
-primes = []
+def left_to_right():
+    biggest_sum = 0
+    current_sum = 1
+    i = 0
+    j = 0
+    count = 1
+    while i < 20:
+        while j < 20:
+            current_sum = current_sum * grid[i][j]
+            if count == 4:
+                if current_sum > biggest_sum:
+                    biggest_sum = current_sum
+                current_sum = 1
+                count = 0
+                j = j - 3
+            count += 1
+            j += 1
+        i += 1
+    return(biggest_sum)
 
-def is_prime(num):
-    prime = True
-    x = 2
-    while prime == True and x < num:
-        if num % x == 0:
-            prime = False
-        x += 1
-    return(prime)
+def right_to_left():
+    pass
 
-def prime_search(s,e):
-    for num in range(s, e):
-        p = is_prime(num)
-        if p == True:
-            primes.append(num)
+def up_down():
+    pass
 
-#val = prime_search(1, 2000000)
-#print(primes, sum(primes))
+def down_up():
+    pass
 
+def nw_to_se():
+    pass
 
-########## Using Sieve of Eratosthenes algorithm ##########
-total = 0
-num = 2000000
+def se_to_nw():
+    pass
 
-prime_numbers = [True for i in range(num + 1)]
+def sw_to_ne():
+    pass
 
-p = 2
-while p * p <= num:
-    if prime_numbers[p] == True:
-        for i in range(p ** 2, num + 1, p):
-            prime_numbers[i] = False
-    p += 1
+def ne_to_sw():
+    pass
 
-for p in range(2, num + 1):
-    if prime_numbers[p] == True:
-        print(p)
-        total = total + p
-
-print(total)
+print(left_to_right())
